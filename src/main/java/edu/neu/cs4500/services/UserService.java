791be83bcef170b85a75739696221624a7da273f
jmagrath <magrath.j@husky.neu.edu>
2019-01-15 14:21:16 -0500

    Added findUserById method and changed users declaration type

--- src/main/java/edu/neu/cs4500/services/UserService.java
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -2,13 +2,15 @@ package edu.neu.cs4500.services;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.List;
 import java.util.ArrayList;
 import edu.neu.cs4500.models.*;
 
 @RestController
 public class UserService {
-	static List<User> users = new ArrayList();
+	static List<User> users = new ArrayList<User>();
 	static {
 		users.add(new User(123, "alice", "alice", "Alice", "Wonderland"));
 		users.add(new User(234, "bob", "bob", "Bob", "Marley"));
@@ -16,5 +18,16 @@ public class UserService {
 	@GetMapping("/api/user")
 	public List<User> findAllUsers() {
 		return users;
+	}
+	
+	@GetMapping("/api/user/{userId}")
+	public User findUserById(
+		@PathVariable("userId") Integer id) {
+		for(User user: users) {
+			if(user.getId() == id) {
+				return user;
+			}
+		}
+		return null;
 	}
 }
