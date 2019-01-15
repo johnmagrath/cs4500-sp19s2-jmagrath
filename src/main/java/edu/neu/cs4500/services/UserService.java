0f8b5616b3aa43a5db6ab891dcb4c7c3217c54ef
jmagrath <magrath.j@husky.neu.edu>
2019-01-15 14:08:05 -0500

    Added findAllUsers get request method

--- src/main/java/edu/neu/cs4500/services/UserService.java
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -1,6 +1,7 @@
 package edu.neu.cs4500.services;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 import java.util.ArrayList;
 import edu.neu.cs4500.models.*;
@@ -11,5 +12,9 @@ public class UserService {
 	static {
 		users.add(new User(123, "alice", "alice", "Alice", "Wonderland"));
 		users.add(new User(234, "bob", "bob", "Bob", "Marley"));
+	}
+	@GetMapping("/api/user")
+	public List<User> findAllUsers() {
+		return users;
 	}
 }
